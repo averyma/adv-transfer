5fd519cec2d439f96e3291627e640c37a253ce15
ama <avery.mabojie@gmail.com>
2023-09-15 16:02:15 -0400

    added my own implementation of kl

--- distiller_zoo/KD.py
+++ distiller_zoo/KD.py
@@ -15,3 +15,15 @@ class DistillKL(nn.Module):
         p_t = F.softmax(y_t/self.T, dim=1)
         loss = F.kl_div(p_s, p_t, size_average=False) * (self.T**2) / y_s.shape[0]
         return loss
+
+class SymmetricKL(nn.Module):
+    '''This is my own implementation of distillation using kl'''
+    def __init__(self):
+        super(SymmetricKL, self).__init__()
+        self.kl_loss = nn.KLDivLoss(reduction="batchmean", log_target=True)
+
+    def forward(self, p_s, p_w):
+        yp_s = F.log_softmax(p_s, dim=1)
+        yp_w = F.log_softmax(p_w, dim=1)
+        loss = self.kl_loss(yp_s, yp_w) + self.kl_loss(yp_w, yp_s)
+        return loss
