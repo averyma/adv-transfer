9be533389208461a2dd2d3f916ae0207d99734c5
ama <avery.mabojie@gmail.com>
2023-12-08 20:26:51 -0500

    implemented avg/min/max over witness models during KL

--- distiller_zoo/KD.py
+++ distiller_zoo/KD.py
@@ -3,17 +3,44 @@ from __future__ import print_function
 import torch.nn as nn
 import torch.nn.functional as F
 
+# class DistillKL(nn.Module):
+    # """Distilling the Knowledge in a Neural Network"""
+    # def __init__(self, T):
+        # super(DistillKL, self).__init__()
+        # self.T = T
+
+    # def forward(self, y_s, y_t):
+        # p_s = F.log_softmax(y_s/self.T, dim=1)
+        # p_t = F.softmax(y_t/self.T, dim=1)
+        # loss = F.kl_div(p_s, p_t, size_average=False) * (self.T**2) / y_s.shape[0]
+        # return loss
 
 class DistillKL(nn.Module):
     """Distilling the Knowledge in a Neural Network"""
-    def __init__(self, T):
+    def __init__(self, T, reduction='average'):
         super(DistillKL, self).__init__()
         self.T = T
+        self.reduction = reduction
+        assert reduction in ['average', 'max', 'min']
 
     def forward(self, y_s, y_t):
         p_s = F.log_softmax(y_s/self.T, dim=1)
-        p_t = F.softmax(y_t/self.T, dim=1)
-        loss = F.kl_div(p_s, p_t, size_average=False) * (self.T**2) / y_s.shape[0]
+        p_t = F.softmax(y_t/self.T, dim=2)
+
+        loss_per_dim_per_sample_per_model = F.kl_div(p_s, p_t, reduction='none') * (self.T**2)
+        loss_per_sample_per_model = loss_per_dim_per_sample_per_model.sum(dim=2)
+
+        if self.reduction == 'average':
+            # take the average over models:
+            loss_per_sample = loss_per_sample_per_model.mean(dim=0)
+        elif self.reduction == 'max':
+            # take the max over models:
+            loss_per_sample = loss_per_sample_per_model.max(dim=0)[0]
+        elif self.reduction == 'min':
+            # take the min over models:
+            loss_per_sample = loss_per_sample_per_model.min(dim=0)[0]
+
+        loss = loss_per_sample.mean()
         return loss
 
 class SymmetricKL(nn.Module):
